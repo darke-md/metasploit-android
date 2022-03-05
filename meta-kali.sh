24a4931252b211d18ef721ec5ab2800ec567ad9e
Dark3 Hackers <97789683+dark3hackers@users.noreply.github.com>
2022-03-06 01:51:39 +0530

    Update meta-kali.sh

--- meta-kali.sh
+++ meta-kali.sh
@@ -2,6 +2,7 @@
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
+cd $HOME
 echo -e "${RED}[!]${ENDCOLOR}${GREEN}installing........Wget${ENDCOLOR}"; pkg install wget -y &> /dev/null
 echo -e "${RED}installing........OpenSsl Tool${ENDCOLOR}"; pkg install openssl-tool -y &> /dev/null
 echo -e "${RED}[!]${ENDCOLOR}${GREEN}installing........Proot${ENDCOLOR}"; pkg install proot -y &> /dev/null
@@ -10,7 +11,12 @@ echo -e "${GREEN}[!]${ENDCOLOR}${RED}Installing Kali Linux...${ENDCOLOR}"; wget 
 echo -e "${GREEN}[!]${ENDCOLOR}${RED}Downloading...Rootfs${ENDCOLOR}"; 
 cd $HOME
 bash kali.sh
-bash start-kali.sh
-
+echo "bash start-kali.sh" > kali
+mv kali /data/data/com.termux/files/usr/bin
+cd data/data/com.termux/files/usr/bin
+chmod +x kali
+cd $HOME
+echo "Making it easier to start kali linux"
+echo "Start kali-linux by typing 'kali'"
 
 
