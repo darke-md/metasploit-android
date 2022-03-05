0ce89820bd8e574d18f7a48e480a97da5fa2d81d
Dark3 Hackers <97789683+dark3hackers@users.noreply.github.com>
2022-03-06 01:35:01 +0530

    meta-kali.sh

--- metasploit-android.sh
+++ metasploit-android.sh
@@ -7,7 +7,7 @@ echo -e "${RED}installing........OpenSsl Tool${ENDCOLOR}"; pkg install openssl-t
 echo -e "${RED}[!]${ENDCOLOR}${GREEN}installing........Proot${ENDCOLOR}"; pkg install proot -y &> /dev/null
 hash -r
 echo -e "${GREEN}[!]${ENDCOLOR}${RED}Installing Kali Linux...${ENDCOLOR}"; wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh &> /dev/null
-echo -e "${GREEN}[!]${ENDCOLOR}${RED}Downloading...Rootfs${ENDCOLOR}"; 
+echo -e "${GREEN}[!]${ENDCOLOR}${RED}Downloading...Rootfs${ENDCOLOR}";
 cd $HOME
 bash kali.sh
 bash start-kali.sh
